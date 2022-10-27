4db1c2f6d0f9c7999a85bd734276f31520d52d9a
OVOMARK <mcncstatefan@icloud.com>
2022-10-27 14:33:39 -0400

    more frame work

--- Develop/index.js
+++ Develop/index.js
@@ -5,33 +5,91 @@ const fs = require('fs');
 const questions = [
     {
         type: 'input',
-        message: 'What is your name?',
-        name: 'firstName'
+        message: 'What is project title?',
+        name: 'Title'
     },
     {
         type: 'input',
-        message: 'What is your favorite food?',
-        name: 'Food'
+        message: 'Enter a description for your porject',
+        name: 'Description'
     },
     {
         type: 'input',
-        message: 'What is your GitHub?',
-        name: 'Github'
+        message: 'Enter installation instructions for your project',
+        name: 'installationInstructions'
     },
     {
         type: 'input',
-        message: 'Where are you from?',
-        name: 'Location'
+        message: 'Enter usage information for your project',
+        name: 'usageInformation'
+    },
+    {
+        type: 'input',
+        message: 'Enter contribution guidelines for your project',
+        name: 'contributionGuidelines'
+    },
+    {
+        type: 'input',
+        message: 'Enter test instructions for your project',
+        name: 'testInstructions'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license would you like?',
+        choices: ['MIT', 'Other', 'GPLv2', 'Apache'],
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, response) {
-    fs.writeFile('/Users/joe/test.txt', content, err => {
+function writeToFile(fileName, answer) {
+    const {Title, Description, installationInstructions, usageInformation, contributionGuidelines, testInstructions, license} = answer;
+
+    switch(license) {
+        case 'MIT':
+            return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+            break;
+        case 'Other':
+            return 'Monday';
+            break;
+        case 'GPLv2':
+            return '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+            break;
+        case 'Apache':
+            return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+            break;
+        default:
+            'N/A'
+    }
+    console.log(license);
+    console.log(fileName);
+    let content = 
+        `# Title
+        ${Title}
+
+        ## Description
+        ${Description}
+        
+        ## Installation Instructions
+        ${installationInstructions}
+        
+        ## Usage
+        ${usageInformation}
+        
+        ## Contribution Guidelines
+        ${contributionGuidelines}
+        
+        ## Test Instructions
+        ${testInstructions}
+        
+        ## License
+        ${license}`
+    fs.writeFile('README.md', content, err => {
         if (err) {
           console.error(err);
         }
         // file written successfully
+        console.log('success')
       });
 }
     
@@ -39,29 +97,11 @@ function writeToFile(fileName, response) {
 function init() {
     const fileName = 'README.md'
     Inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'What is your name?',
-                name: 'firstName'
-            },
-            {
-                type: 'input',
-                message: 'What is your favorite food?',
-                name: 'Food'
-            },
-            {
-                type: 'input',
-                message: 'What is your GitHub?',
-                name: 'Github'
-            },
-            {
-                type: 'input',
-                message: 'Where are you from?',
-                name: 'Location'
-            } 
-        ])
-        .then((answer) => console.log(answer)
+        .prompt(questions)
+        .then((answer) => 
+            
+        writeToFile(fileName, answer)
+            
             
   );
 }
